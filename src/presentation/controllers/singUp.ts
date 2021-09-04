9d6ed49666cbcbdcf399585a132c295bd64891b6
RaphaelCaputo2 <faelelf2@gmail.com>
2021-09-04 19:12:54 -0300

    Fixx eslint

--- src/presentation/controllers/singUp.ts
+++ src/presentation/controllers/singUp.ts
@@ -1,8 +1,16 @@
 export class SingupController {
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
-    };
+    if (!httpRequest.body.name || !httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param'),
+      };
+    }
+    if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      return {
+        statusCode: 404,
+        body: new Error('Wrong Password'),
+      };
+    }
   }
 }
--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -12,6 +12,34 @@ describe('SingUp Controller', () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+    expect(httpResponse.body).toEqual(new Error('Missing param'));
+  });
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SingupController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        // email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param'));
+  });
+  test('Should return 400 if no password is equal', () => {
+    const sut = new SingupController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'anypassword',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(404);
+    expect(httpResponse.body).toEqual(new Error('Wrong Password'));
   });
 });
