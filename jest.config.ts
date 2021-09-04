6bce81910a0e59272d6f203e0a64d12773922d85
RaphaelCaputo2 <faelelf2@gmail.com>
2021-09-04 14:27:15 -0300

    API-04: Add jest test typescript

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  coverageDirectory: 'coverage',
+
+  coverageProvider: 'v8',
+
+  testEnvironment: 'jest-environment-node',
+
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
