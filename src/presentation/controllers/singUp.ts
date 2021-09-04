c9a96e73cc3a8ff5790ca4c84e5e8e395ea89475
RaphaelCaputo2 <faelelf2@gmail.com>
2021-09-04 19:29:00 -0300

    fix eslint erros

--- src/presentation/controllers/singUp.ts
+++ src/presentation/controllers/singUp.ts
@@ -1,16 +1,16 @@
 export class SingupController {
-  handle(httpRequest: any): any {
+  handle (httpRequest: any): any {
     if (!httpRequest.body.name || !httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param'),
-      };
+        body: new Error('Missing param')
+      }
     }
     if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
       return {
         statusCode: 404,
-        body: new Error('Wrong Password'),
-      };
+        body: new Error('Wrong Password')
+      }
     }
   }
 }
--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,45 +1,45 @@
-import { SingupController } from './singUp';
+import { SingupController } from './singUp'
 describe('SingUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SingupController();
+    const sut = new SingupController()
     const httpRequest = {
       body: {
         // name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param'));
-  });
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param'))
+  })
   test('Should return 400 if no email is provided', () => {
-    const sut = new SingupController();
+    const sut = new SingupController()
     const httpRequest = {
       body: {
         name: 'any_name',
         // email: 'any_email@mail.com',
         password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param'));
-  });
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param'))
+  })
   test('Should return 400 if no password is equal', () => {
-    const sut = new SingupController();
+    const sut = new SingupController()
     const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
-        passwordConfirmation: 'anypassword',
-      },
-    };
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(404);
-    expect(httpResponse.body).toEqual(new Error('Wrong Password'));
-  });
-});
+        passwordConfirmation: 'anypassword'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(404)
+    expect(httpResponse.body).toEqual(new Error('Wrong Password'))
+  })
+})
