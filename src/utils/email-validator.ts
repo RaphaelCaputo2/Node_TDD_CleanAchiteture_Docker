da645553862a812e014730dccb692432504db22b
RaphaelCaputo2 <faelelf2@gmail.com>
2021-09-06 23:19:47 -0300

    Email Validator adapter

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,6 @@
+import { EmailValidator } from '../presentation/protocols/email-validator'
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return false
+  }
+}
