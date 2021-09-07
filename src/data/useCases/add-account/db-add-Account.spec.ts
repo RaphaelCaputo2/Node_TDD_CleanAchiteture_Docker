d527bfd9e0874ca4702e2b2c2bcc7f5e25fa02c9
RaphaelCaputo2 <faelelf2@gmail.com>
2021-09-07 14:00:33 -0300

    Create new interface and class db-account and encrypter password test

---
+++ src/data/useCases/add-account/db-add-Account.spec.ts
@@ -0,0 +1,20 @@
+import { DbAddAccount } from './db-add-account'
+describe('DbAcount UseCase', () => {
+  test('Should call Encrypter with correct password', async () => {
+    class EncrypterStub {
+      async encrypt (value: string): Promise<string> {
+        return await new Promise(resolve => resolve('hashed_value'))
+      }
+    }
+    const encrypterStub = new EncrypterStub()
+    const sut = new DbAddAccount(encrypterStub)
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    await sut.add(accountData)
+    expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+  })
+})
