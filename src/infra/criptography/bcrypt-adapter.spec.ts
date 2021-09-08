6997db4aa48fea5f1545fc83b23b5c2869f30f94
RaphaelCaputo2 <faelelf2@gmail.com>
2021-09-07 22:19:28 -0300

    Remove dependecies in test

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -5,18 +5,26 @@ jest.mock('bcrypt', () => ({
     return await new Promise(resolve => resolve('hash'))
   }
 }))
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct value', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(12)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
+    const sut = makeSut()
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
   test('Should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+  // test('Should throw if bcrypt throws', async () => {
+  //   const sut = makeSut()
+  //   jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+  //   const promise = sut.encrypt('any_value')
+  //   await expect(promise).rejects.toThrow()
+  // })
 })
--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -8,7 +8,6 @@ export class BcryptAdapter implements Encrypter {
 
   async encrypt (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
-
     return hash
   }
 }
